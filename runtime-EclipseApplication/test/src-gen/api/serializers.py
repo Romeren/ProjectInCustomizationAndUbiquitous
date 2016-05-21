eab20aaeb4159faf9531203e26ad71aa5fa32bf8
Nikolaj Schaldemose Reibke <nire@tv2.dk>
2016-05-21 21:56:11 +0200

    buhu

--- runtime-EclipseApplication/test/src-gen/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from api.model import api
-from django.contrib.auth.models import User
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-		fields = ('url', 'username', 'email', 'is_staff')
--- runtime-EclipseApplication/test/src-gen/api/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import include,url
-
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
-
